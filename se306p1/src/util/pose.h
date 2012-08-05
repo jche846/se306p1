04d4cca6f9c08ce3dda77a225c1223ae2ed56480
Scott Goodew (sgoo052) <scott.goodhew0@gmail.com>
2012-08-05 12:48:13 +1200

    Fix copy constructor of Pose

--- se306p1/src/util/pose.h
+++ se306p1/src/util/pose.h
@@ -1,15 +1,16 @@
+#pragma once
 #include "vector2.h"
 
 namespace se306p1 {
   class Pose {
   public:
-    const Vector2 position_;
-    const double theta_;
+    Vector2 position_;
+    double theta_;
 
     inline Pose(Vector2 position, double theta) :
       position_(position), theta_(theta) { }
 
-    inline Pose(Pose &from) :
+    inline Pose(const Pose &from) :
       position_(from.position_), theta_(from.theta_) { }
   };
 }
