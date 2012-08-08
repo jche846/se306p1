2cb0d780a300c93a7db7462f36536835301ce4fc
Sam Grace <smrgrace@gmail.com>
2012-08-08 16:55:03 +1200

    add Robot behaviour to RobotSupervisor::Run to begin moving in a circle

--- se306p1/src/supervisor/rotate_supervisor.cpp
+++ se306p1/src/supervisor/rotate_supervisor.cpp
@@ -5,36 +5,103 @@
 
 #define ROBOT_WIDTH 0.35
 #define DEFAULT_MOVE_SPEED 1
+#define CIRCLE_LV 2
+#define CIRCLE_AV 2
 
 namespace se306p1 {
-  RotateSupervisor::~RotateSupervisor() {  }
+  RotateSupervisor::~RotateSupervisor() {
+  }
 
   void RotateSupervisor::Run() {
     ROS_INFO("Starting rotate supervisor.");
     ros::Rate r(100);
 
-    // TODO: remove this code!
-    for(std::pair<const uint64_t, std::shared_ptr<Robot>> &pair : this->robots_) {
-      pair.second->Do(2.0, 2.0, false);
-    }
+    this->FindRobotDests();
+    this->MoveNodesToDests(this->nonHeadRobots_, this->lineLocations_);
+
+  // Scotts Instructions for Sam and Luey:
+  // * start cluster head moving
+  // * Go other 5 to cluster head
+  // * Queue Do on other 5 to move in circle
+
+    bool rotating = false;
 
     while (ros::ok()) {
       this->DispatchMessages();
+
+      if (!rotating) {
+        // iterate through all robots and see if they have finished executing
+        bool allFinishedExecuting = true;
+        std::map<uint64_t, std::shared_ptr<Robot> >::iterator it;
+        //if any robot is executing, set allFinishedExecuting to false
+        for (it = this->robots_.begin(); it != this->robots_.end(); it++) {
+          std::shared_ptr<Robot> robot_ptr = it->second;
+          if (robot_ptr->executing_ == true) {
+            allFinishedExecuting = false;
+            break;
+          }
+        }
+
+        // tell all robots to move to clusterHeadPosition
+        // and enqueue them moving in a circle.
+        if (allFinishedExecuting) {
+          for (it = this->robots_.begin(); it != this->robots_.end(); it++) {
+            std::shared_ptr<Robot> robot_ptr = it->second;
+            robot_ptr->Go(this->clusterHead_->pose_, false); //moveTocluster head position
+            robot_ptr->Do(CIRCLE_LV, CIRCLE_AV, true); //queue circling
+          }
+          rotating = true;
+        }
+      }
+
       r.sleep();
       ros::spinOnce();
+
     }
-
-    //this->FindRobotDests();
-    //this->MoveNodesToDests(this->nonHeadRobots_, this->lineLocations_);
-
-    //wait for robots to arrive in line
-
-    // start cluster head moving
-
-    // Go other 5 to cluster head
-
-    // Queue Do on other 5 to move in circle
   }
+
+  //    // Wait for robots to arrive in line and be rotated in the correct direction
+  //    map<uint64_t, std::shared_ptr<Robot>>robots_::iterator rit;   // Iterate through all the robots
+  //
+  //    while(rit != rit.end()) {
+  //      if (rit->location != lineLocations_ || rit->direction != pointtowardsorigin) {
+  //        continue;
+  //      } else {
+  //        rit++;
+  //      }
+  //    }
+
+
+  //std::map<uint64_t, std::shared_ptr<Robot> >::iterator it;
+  //
+  //for (it = this->robots_.begin(); it != this->robots_.end(); it++) {
+  //
+  //  std::shared_ptr<Robot> robot_ptr = (*it).second;
+  //
+  //  executing_
+  //
+  //  while (robot_ptr->pose_.theta_ != theta) {
+  //    //wait
+  //    continue;
+  //  }
+
+  //      double distToOrig = robot_ptr->pose_.position_.Length();
+  //      if (clusterHeadDist == -1) {
+  //        this->clusterHead_ = robot_ptr;
+  //        clusterHeadDist = distToOrig;
+  //      } else {
+  //        if (distToOrig != 0 && distToOrig < clusterHeadDist) {
+  //
+  //          this->nonHeadRobots_.push_back(this->clusterHead_);
+  //
+  //          this->clusterHead_ = robot_ptr;
+  //          clusterHeadDist = distToOrig;
+  //        } else {
+  //          this->nonHeadRobots_.push_back(robot_ptr);
+  //        }
+  //      }
+
+
 
   void RotateSupervisor::FindRobotDests() {
     size_t numRobots = this->robots_.size();
@@ -43,7 +110,7 @@ namespace se306p1 {
 
     Vector2 lastLocation = this->clusterHead_->pose_.position_;
     Vector2 robotSep = lastLocation.Normalized() * ROBOT_WIDTH * 6;
-    for(size_t i = 1; i < numRobots; i++) {
+    for (size_t i = 1; i < numRobots; i++) {
       lastLocation = lastLocation + robotSep;
       this->lineLocations_.push_back(Pose(lastLocation, theta));
     }
