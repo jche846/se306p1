7187e3580387fa0fe0d8f89a50a69cd37f18a2c4
Tony Young <dyou706@aucklanduni.ac.nz>
2012-08-08 20:42:20 +1200

    Add launcher for creating arbitrary amounts of robots.

---
+++ generate_world.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import random
+import sys
+
+COLORS = ["blue", "green", "black", "yellow", "red", "brown"]
+
+HEADER = """\
+define mylaser laser
+(
+  range_max 5.0
+  fov 180
+  samples 180
+  color "black"
+  size [ 0.05 0.05 0.1 ]
+)
+
+define myrobot position
+(
+  size [0.35 0.35 0.25]
+  drive "diff"
+  mylaser(pose [ 0.050 0.000 0 0.000 ])
+)
+
+define floorplan model
+(
+  color "gray30"
+  boundary 1
+  laser_return 1
+)
+
+resolution 0.02
+interval_sim 100
+
+window
+( 
+  size [ 745.000 448.000 ]
+  scale 30
+)
+
+floorplan
+( 
+  name "swarm"
+  bitmap "swarm_world.pgm"
+  size [ 54.0 58.7 0.5 ]
+  pose [ -29.350 27.000 0 90.000 ]
+)
+
+"""
+
+def generate(n):
+    robots = []
+
+    for i in range(n):
+        robots.append("""\
+myrobot
+(
+  name "r{n}"
+  pose [ {x} {y} 0 0 ]
+  color "{color}"
+)
+""".format(
+            n=i,
+            x=random.randint(-30, 30),
+            y=random.randint(-30, 30),
+            color=COLORS[i % len(COLORS)]
+        ))
+
+    return HEADER + "\n".join(robots)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.stderr.write("usage: {} NUM_ROBOTS\n".format(sys.argv[0]))
+        sys.exit(1)
+
+    print(generate(int(sys.argv[1])))
+
--- launch.sh
+++ launch.sh
@@ -4,13 +4,13 @@
 # Start the ROS core, controllers and finally the supervisor.
 #
 
-#roscore &
+roscore &
+./generate_world.py $1 > se306p1.world
+rosrun stage stageros se306p1.world &
 se306p1/bin/rotate_supervisor &
 sleep 1
-exec -a robot_controller_${RANDOM} se306p1/bin/robot_controller _rid:=0 &
-exec -a robot_controller_${RANDOM} se306p1/bin/robot_controller _rid:=1 &
-exec -a robot_controller_${RANDOM} se306p1/bin/robot_controller _rid:=2 &
-exec -a robot_controller_${RANDOM} se306p1/bin/robot_controller _rid:=3 &
-exec -a robot_controller_${RANDOM} se306p1/bin/robot_controller _rid:=4 &
-$1 &
+for i in $(seq 0 $(($1 - 1))); do
+    exec -a robot_controller_${RANDOM} se306p1/bin/robot_controller _rid:=${i} &
+    echo $i
+done
 wait
