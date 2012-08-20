27805def43435af0cd0553564cdc6fe1f2579994
Tony Young <dyou706@aucklanduni.ac.nz>
2012-08-20 22:51:43 +1200

    Use unions to represent command variants.

--- se306p1/src/util/command.h
+++ se306p1/src/util/command.h
@@ -16,13 +16,26 @@ enum class CommandType {
 
 class Command {
  public:
-  bool enqueue;
   CommandType type;
-  double lv;
-  double av;
-  double x;
-  double y;
-  double theta;
+  bool enqueue;
+
+  union {
+    // NONE
+    struct {};
+
+    // DO
+    struct {
+      double lv;
+      double av;
+    };
+
+    // GO
+    struct {
+      double x;
+      double y;
+      double theta;
+    };
+  };
 
   /**
    * A null command.
@@ -30,51 +43,32 @@ class Command {
   Command() {
     this->enqueue = false;
     this->type = CommandType::NONE;
-    this->x = std::numeric_limits<double>::quiet_NaN();
-    this->y = std::numeric_limits<double>::quiet_NaN();
-    this->theta = std::numeric_limits<double>::quiet_NaN();
-    this->lv = std::numeric_limits<double>::quiet_NaN();
-    this->av = std::numeric_limits<double>::quiet_NaN();
   }
 
   /**
    * Constructor for Do commands
    */
   Command(Do msg) {
-    this->x = std::numeric_limits<double>::quiet_NaN();
-    this->y = std::numeric_limits<double>::quiet_NaN();
-    this->theta = std::numeric_limits<double>::quiet_NaN();
+    this->type = CommandType::DO;
     this->lv = msg.lv;
     this->av = msg.av;
     this->enqueue = msg.enqueue;
-
-    // CommandType signifies if this->command is a Do or a Go.
-    this->type = CommandType::DO;
   }
 
   /**
    * Constructor for Go commands
    */
   Command(Go msg) {
+    this->type = CommandType::GO;
     this->x = msg.x;
     this->y = msg.y;
     this->theta = msg.theta;
     this->enqueue = msg.enqueue;
-
-    this->lv = std::numeric_limits<double>::quiet_NaN();
-    this->av = std::numeric_limits<double>::quiet_NaN();
-
-    this->type = CommandType::GO;
   }
 
   Command(Scan msg) {
     this->type = CommandType::SCAN;
     this->enqueue = msg.enqueue;
-    this->x = std::numeric_limits<double>::quiet_NaN();
-    this->y = std::numeric_limits<double>::quiet_NaN();
-    this->theta = std::numeric_limits<double>::quiet_NaN();
-    this->lv = std::numeric_limits<double>::quiet_NaN();
-    this->av = std::numeric_limits<double>::quiet_NaN();
   }
 
   virtual ~Command() {
