0d7f4b89415cc04453ee6b11ed7bd21433635fb9
jcgharvey <jourdan.harvey@gmail.com>
2012-08-21 20:47:57 +1200

    Initiate Supervisor testing

--- se306p1/test/supervisor.cpp
+++ se306p1/test/supervisor.cpp
@@ -1,5 +1,37 @@
 #include <gtest/gtest.h>
+#include <supervisor.h>
 
+//Begin to implement tests in supervisor
+using namespace se306p1;
+namespace {
+class SupervisorTest : public testing::Test {
+   
+   public:
+    
+    SupervisorTest() : nh("~"), rc(nh,1) {
+      
+    }
+
+    virtual ~SupervisorTest() {
+      
+    }
+
+    virtual void SetUp() {
+
+    }
+
+    virtual void TearDown() {
+      
+    }
+  };
+
+  /**
+  * Tests that do_callback runs correctly when enqueue is true
+  */
+  TEST_F(SupervisorTest, test_do_callback_enqueue) {
+    
+  }
+  
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
