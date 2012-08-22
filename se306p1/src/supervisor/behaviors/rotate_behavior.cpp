44fbcdf6a9a738d589cd523857d9828f28d97d50
Andrew Luey <alue004@aucklanduni.ac.nz>
2012-08-22 19:02:47 +1200

    Clean up rotate code.

--- se306p1/src/supervisor/behaviors/rotate_behavior.cpp
+++ se306p1/src/supervisor/behaviors/rotate_behavior.cpp
@@ -2,7 +2,7 @@
 
 #include "../supervisor.h"
 
-#define DEFAULT_MOVE_SPEED 1
+#define DEFAULT_LV 4.0
 
 namespace se306p1 {
 RotateBehavior::RotateBehavior(Supervisor &sup) : Behavior(sup) {
@@ -16,22 +16,17 @@ void RotateBehavior::Execute() {
   ROS_INFO("Rotating.");
 
   Pose clusterHeadPose = this->supervisor_.clusterHeadPose_;
+
   for (auto &cur_robot : this->supervisor_.robots_) {
-    if (cur_robot.second->id_ != this->supervisor_.clusterHead_->id_) {
-      // tell robot to move to cluster head pos
-      cur_robot.second->Go(clusterHeadPose, false);
-    }
-    // enqueue rotate for after they have reached the cluster head pos
+    cur_robot.second->Go(clusterHeadPose, false);
 
     int num_of_robots =  this->supervisor_.robots_.size();
-    double radius = (num_of_robots * 6 * 0.35) / (M_PI * 2);
-
-    double circle_lv = 4.0;
-    double circle_av = circle_lv / (radius);
+    double radius = (num_of_robots * 6.0 * 0.35) / (M_PI * 2.0);
 
     cur_robot.second->Do(
-        circle_lv, circle_av,
-        cur_robot.second->id_ != this->supervisor_.clusterHead_->id_);
+    		DEFAULT_LV,
+    		DEFAULT_LV / radius,
+    		true);
   }
 }
 }
