22ba2e03453f60d61913800022ee49fc7f81abe4
Andrew Hughson <andrew@atshughson.me>
2012-08-22 21:07:52 +1200

    Add defaults to command line arguments. Indentation tidy

--- generate_world.py
+++ generate_world.py
@@ -5,8 +5,6 @@ import math
 import sys
 import os
 
-COLORS = ["blue", "green", "black", "yellow", "red", "brown"]
-
 HEADER = """\
 resolution 0.02
 interval_sim 100
@@ -56,67 +54,78 @@ robot_positions = []
 
 
 def append_robot(robots, n, x, y):
-    robots.append("""\
+  robots.append("""\
 myrobot
 (
   name "r{i}"
   pose [ {x} {y} 0 0 ]
-  color "{color}"
+  color_rgba [ {r} {g} {b} 1.0 ]
 )
+
 """.format(
-        i=len(robots), x=x, y=y, color=COLORS[n % len(COLORS)]
-    ))
+    i=len(robots), x=x, y=y,
+    r=random.randint(0, 255),
+    g=random.randint(0, 255),
+    b=random.randint(0, 255)
+  ))
 
 
 def append_barcodes():
   barcodes = []
-  count = 0
-  for filename in os.listdir("images"):
+
+  for i, filename in enumerate(os.listdir("images")):
     barcodes.append("""\
 barcode
 (
   name "{name}"
-  bitmap "images/{fileName}"
+  bitmap "images/{image_file}"
   size [ 6 4.5 0.5 ]
   pose [ 60.0 {y}.0 0.0 0.0 ]
 )
+
 """.format(
-        name=filename.replace(".pgm", "", 1), fileName=filename, y=count * 8
-  ))
-    count += 1
+        name=filename.replace(".pgm", "", 1), image_file=filename, y=i * 8
+    ))
+
   return barcodes
 
 
-def generate(n, gn):
-    robots = []
+def generate(num_robots, num_groups):
+  robots = []
 
-    for i in range(n):
-        robot_positions.append((random.randint(-40, 40),
-                                random.randint(-40, 40)))
+  for i in range(num_robots):
+    robot_positions.append((random.randint(-40, 40),
+                            random.randint(-40, 40)))
 
-    robot_positions.sort(key=lambda pos: math.sqrt(pos[0] * pos[0] + pos[1] * pos[1]))
+  robot_positions.sort(key=lambda pos: math.sqrt(pos[0] * pos[0] + pos[1] * pos[1]))
 
-    cluster_heads = robot_positions[:gn]
-    members = robot_positions[gn:]
-    members.reverse()
+  cluster_heads = robot_positions[:num_groups]
+  members = robot_positions[num_groups:]
+  members.reverse()
 
-    num_members = int(round(n / float(gn)))
+  num_members = num_robots / num_groups
 
-    for i, (x, y) in enumerate(cluster_heads):
-        append_robot(robots, i, x, y)
+  for i, (x, y) in enumerate(cluster_heads):
+    append_robot(robots, i, x, y)
 
-        for _ in range(num_members - 1):
-            if not members:
-              break
-            x, y = members.pop()
+    for _ in range(num_members - 1):
+      if not members:
+        break
+      x, y = members.pop()
 
-            append_robot(robots, i, x, y)
+      append_robot(robots, i, x, y)
 
-    return HEADER + "\n".join(robots) + "\n".join(append_barcodes())
+  return HEADER + "\n".join(robots) + "\n".join(append_barcodes())
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        sys.stderr.write("usage: {} NUM_ROBOTS NUM_GROUPS\n".format(sys.argv[0]))
-        sys.exit(1)
+  if len(sys.argv) == 3:
+    num_robots = int(sys.argv[1])
+    num_groups = int(sys.argv[2])
+  elif len(sys.argv == 1):
+    num_robots = 24
+    num_groups = 4
+  else:
+    sys.stderr.write("usage: {} NUM_ROBOTS (default: 24) NUM_GROUPS (default: 6)\n".format(sys.argv[0]))
+    sys.exit(1)
 
-    print(generate(int(sys.argv[1]), int(sys.argv[2])))
+  print(generate(num_robots, num_groups))
