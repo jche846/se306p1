1fcddb5972935a0e53b4a5cf102070971e34a9ec
jcgharvey <jourdan.harvey@gmail.com>
2012-08-24 10:25:44 +1200

    Electhead test running

--- se306p1/test/supervisor.cpp
+++ se306p1/test/supervisor.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include"../src/supervisor/supervisor.h"
+#include"../src/supervisor/robot.h"
 #include "../src/util/command.h"
 #include "../src/util/vector2.h"
 #include "../src/util/pose.h"
@@ -12,14 +13,8 @@ class SupervisorTest : public testing::Test {
    public:
     ros::NodeHandle nh;
     Supervisor super;
-    std::shared_ptr<Robot> r0;
-    std::shared_ptr<Robot> r1;
-    std::shared_ptr<Robot> r2;
-    std::shared_ptr<Robot> r3;
-    std::shared_ptr<Robot> r4;
-    
+
     SupervisorTest() : nh("~"), super(nh) {
-      
     }
 
     virtual ~SupervisorTest() {
@@ -48,38 +43,28 @@ class SupervisorTest : public testing::Test {
   /**
   * Check that for a selection of 5 robots the cluster head is selected correctly.
   */
-  /*
+  
   TEST_F(SupervisorTest, testElectHeadRandomPositions) {
     // we need to create a set of robots and check
     // that they get assigned to the correct supervisor fields
     //    i.e. to clusterHead and to nonClusterHeads
-    r0 = std::shared_ptr<Robot>(new Robot(0));
-    r1 = std::shared_ptr<Robot>(new Robot(1));
-    r2 = std::shared_ptr<Robot>(new Robot(2));
-    r3 = std::shared_ptr<Robot>(new Robot(3));
-    r4 = std::shared_ptr<Robot>(new Robot(4));
-    r0->pose_ = Pose(Vector2(10.0, 8.0),35.0);
-    r1->pose_ = Pose(Vector2(2.0, 2.0),-45.0);
-    r2->pose_ = Pose(Vector2(-2.0, 5.0),10.0);
-    r3->pose_ = Pose(Vector2(-2.0, -5.0),135.0);
-    r4->pose_ = Pose(Vector2(-6.0, 3.0),-160.0);
+    for (int ID = 0 ; ID < 5 ; ID++ ) {
+      super.robots_[ID] = std::shared_ptr<Robot>(new Robot(ID));
+    }
+    super.robots_[0]->pose_ = Pose(Vector2(10.0, 8.0),35.0);
+    super.robots_[1]->pose_ = Pose(Vector2(2.0, 2.0),-45.0);
+    super.robots_[2]->pose_ = Pose(Vector2(-2.0, 5.0),10.0);
+    super.robots_[3]->pose_ = Pose(Vector2(-2.0, -5.0),135.0);
+    super.robots_[4]->pose_ = Pose(Vector2(-6.0, 3.0),-160.0);
 
-    // add robots to a map.
-    super.robots_[0] = r0;
-    super.robots_[1] = r1;
-    super.robots_[2] = r2;
-    super.robots_[3] = r3;
-    super.robots_[4] = r4;
-    
     //call elect head
     super.ElectHead();
     
-    //check that the clust head is elected correctly.
+    //check that the cluster head is the cluster head near the origin
     ASSERT_EQ(super.clusterHead_->id_,1);
-    
     // Add asserts to check taht the nonHeads contain the correct robots
   }
-  */
+  
   /**
   * Check that a robot created at the origin is not made the cluster head.
   */
@@ -87,32 +72,21 @@ class SupervisorTest : public testing::Test {
     // we need to create a set of robots and check
     // that they get assigned to the correct supervisor fields
     //    i.e. to clusterHead and to nonClusterHeads
-    
-    r0 = std::shared_ptr<Robot>(new Robot(0));
-    r1 = std::shared_ptr<Robot>(new Robot(1));
-    r2 = std::shared_ptr<Robot>(new Robot(2));
-    r3 = std::shared_ptr<Robot>(new Robot(3));
-    r4 = std::shared_ptr<Robot>(new Robot(4));
-    
-    r0.pose_ = Pose(Vector2(0.0, 0.0),35.0);
-    r1.pose_ = Pose(Vector2(2.0, 2.0),-45.0);
-    r2.pose_ = Pose(Vector2(-2.0, 5.0),10.0);
-    r3.pose_ = Pose(Vector2(-2.0, -5.0),135.0);
-    r4.pose_ = Pose(Vector2(-6.0, 3.0),-160.0);
 
-    // add robots to a map.
-    super.robots_[0] = r0;
-    super.robots_[1] = r1;
-    super.robots_[2] = r2;
-    super.robots_[3] = r3;
-    super.robots_[4] = r4;
-    
+    for (int ID = 0 ; ID < 5 ; ID++ ) {
+      super.robots_[ID] = std::shared_ptr<Robot>(new Robot(ID));
+    }
+    super.robots_[0]->pose_ = Pose(Vector2(0.0, 0.0),35.0);
+    super.robots_[1]->pose_ = Pose(Vector2(2.0, 2.0),-45.0);
+    super.robots_[2]->pose_ = Pose(Vector2(-2.0, 5.0),10.0);
+    super.robots_[3]->pose_ = Pose(Vector2(-2.0, -5.0),135.0);
+    super.robots_[4]->pose_ = Pose(Vector2(-6.0, 3.0),-160.0);
+
     //call elect head
     super.ElectHead();
     
     //check that the cluster head is not the robot that started at the origin.
     ASSERT_NE(super.clusterHead_->id_,0);
-    
     
     ASSERT_EQ(super.clusterHead_->id_,1);
     // Add asserts to check taht the nonHeads contain the correct robots
@@ -120,6 +94,7 @@ class SupervisorTest : public testing::Test {
   }
 }//namespace
 int main(int argc, char *argv[]) {
+  ros::init(argc, argv, "test_supervisor");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
