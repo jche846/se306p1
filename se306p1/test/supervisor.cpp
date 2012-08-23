5e59d5c71b42ebba611c14ecd7742fab828f8270
jcgharvey <jourdan.harvey@gmail.com>
2012-08-23 21:47:44 +1200

    Robots in supervisor test are not instantiated correctly

--- se306p1/test/supervisor.cpp
+++ se306p1/test/supervisor.cpp
@@ -1,29 +1,36 @@
 #include <gtest/gtest.h>
-#include <supervisor.h>
-#include <robot.h>
-#include <pose.h>
-#include <Vector2.h>
+#include"../src/supervisor/supervisor.h"
+#include "../src/util/command.h"
+#include "../src/util/vector2.h"
+#include "../src/util/pose.h"
 
 //Begin to implement tests in supervisor
 using namespace se306p1;
-using namespace Supervisor;
 namespace {
 class SupervisorTest : public testing::Test {
    
    public:
+    ros::NodeHandle nh;
+    Supervisor super;
+    std::shared_ptr<Robot> r0;
+    std::shared_ptr<Robot> r1;
+    std::shared_ptr<Robot> r2;
+    std::shared_ptr<Robot> r3;
+    std::shared_ptr<Robot> r4;
     
-    SupervisorTest() {
-      ros::NodeHandle nh("~");
-      Supervisor super(nh);
+    SupervisorTest() : nh("~"), super(nh) {
+      
     }
 
     virtual ~SupervisorTest() {
     }
 
     virtual void SetUp() {
+
     }
 
     virtual void TearDown() {
+      
     }
   };
 
@@ -41,24 +48,21 @@ class SupervisorTest : public testing::Test {
   /**
   * Check that for a selection of 5 robots the cluster head is selected correctly.
   */
-  
-  TEST_F(SupervisorTest, testElectHead) {
+  /*
+  TEST_F(SupervisorTest, testElectHeadRandomPositions) {
     // we need to create a set of robots and check
     // that they get assigned to the correct supervisor fields
     //    i.e. to clusterHead and to nonClusterHeads
-    
-    // create some robots
-    Robot r0(0);
-    Robot r1(1);
-    Robot r2(2);
-    Robot r3(3);
-    Robot r4(4);
-    
-    r0.pose_ = Pose(Vector2(10.0, 8.0),35.0);
-    r1.pose_ = Pose(Vector2(2.0, 2.0),-45.0);
-    r2.pose_ = Pose(Vector2(-2.0, 5.0),10.0);
-    r3.pose_ = Pose(Vector2(-2.0, -5.0),135.0);
-    r4.pose_ = Pose(Vector2(-6.0, 3.0),-160.0);
+    r0 = std::shared_ptr<Robot>(new Robot(0));
+    r1 = std::shared_ptr<Robot>(new Robot(1));
+    r2 = std::shared_ptr<Robot>(new Robot(2));
+    r3 = std::shared_ptr<Robot>(new Robot(3));
+    r4 = std::shared_ptr<Robot>(new Robot(4));
+    r0->pose_ = Pose(Vector2(10.0, 8.0),35.0);
+    r1->pose_ = Pose(Vector2(2.0, 2.0),-45.0);
+    r2->pose_ = Pose(Vector2(-2.0, 5.0),10.0);
+    r3->pose_ = Pose(Vector2(-2.0, -5.0),135.0);
+    r4->pose_ = Pose(Vector2(-6.0, 3.0),-160.0);
 
     // add robots to a map.
     super.robots_[0] = r0;
@@ -71,25 +75,24 @@ class SupervisorTest : public testing::Test {
     super.ElectHead();
     
     //check that the clust head is elected correctly.
-    ASSERT_EQ(super.clusterHead_.id_,0);
+    ASSERT_EQ(super.clusterHead_->id_,1);
     
     // Add asserts to check taht the nonHeads contain the correct robots
   }
-  
+  */
   /**
   * Check that a robot created at the origin is not made the cluster head.
   */
-  TEST_F(SupervisorTest, testElectHead) {
+  TEST_F(SupervisorTest, testElectHeadWithARobotAtOrigin) {
     // we need to create a set of robots and check
     // that they get assigned to the correct supervisor fields
     //    i.e. to clusterHead and to nonClusterHeads
     
-    // create some robots
-    Robot r0(0);
-    Robot r1(1);
-    Robot r2(2);
-    Robot r3(3);
-    Robot r4(4);
+    r0 = std::shared_ptr<Robot>(new Robot(0));
+    r1 = std::shared_ptr<Robot>(new Robot(1));
+    r2 = std::shared_ptr<Robot>(new Robot(2));
+    r3 = std::shared_ptr<Robot>(new Robot(3));
+    r4 = std::shared_ptr<Robot>(new Robot(4));
     
     r0.pose_ = Pose(Vector2(0.0, 0.0),35.0);
     r1.pose_ = Pose(Vector2(2.0, 2.0),-45.0);
@@ -108,12 +111,14 @@ class SupervisorTest : public testing::Test {
     super.ElectHead();
     
     //check that the cluster head is not the robot that started at the origin.
-    ASSERT_NE(super.clusterHead_.id_,0);
+    ASSERT_NE(super.clusterHead_->id_,0);
     
+    
+    ASSERT_EQ(super.clusterHead_->id_,1);
     // Add asserts to check taht the nonHeads contain the correct robots
     
   }
-  
+}//namespace
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
